34a3bfdfb01e9c7638ccbbf30ec45f4192991dea
ryane <ryanesc@gmail.com>
2013-05-19 09:00:55 -0400

    add a script to install the tomorow theme in VIM

---
+++ install-tomorrow-theme.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/.vim/colors
+pushd ~/.vim/colors
+curl -O https://raw.github.com/chriskempson/vim-tomorrow-theme/master/colors/Tomorrow-Night-Blue.vim
+curl -O https://raw.github.com/chriskempson/vim-tomorrow-theme/master/colors/Tomorrow-Night-Bright.vim
+curl -O https://raw.github.com/chriskempson/vim-tomorrow-theme/master/colors/Tomorrow-Night-Eighties.vim
+curl -O https://raw.github.com/chriskempson/vim-tomorrow-theme/master/colors/Tomorrow-Night.vim
+curl -O https://raw.github.com/chriskempson/vim-tomorrow-theme/master/colors/Tomorrow.vim
+popd
