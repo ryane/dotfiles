6f0013993ec43fadc762781ddf316af43dd3edab
ryane <ryanesc@gmail.com>
2013-10-23 13:15:36 -0400

    add new method to offlineimap.py to get pw from encrypted authinfo file

--- offlineimap.py
+++ offlineimap.py
@@ -1,6 +1,6 @@
 # https://github.com/garybernhardt/dotfiles/blob/master/.offlineimap.py
 
-import os
+import re, os
 
 def get_imap_passwd():
   cmd = "/usr/bin/security 2>&1 >/dev/null find-generic-password -s \"Gmail Offlineimap Application Password\" -g"
@@ -10,3 +10,9 @@ def get_imap_passwd():
     return passwds[1][1:-1]
   else:
     return ""
+
+def get_auth_info_pw(machine, login, port):
+  s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
+  p = re.compile(s)
+  authinfo = os.popen("gpg -q --no-tty -d ~/.authinfo.gpg").read()
+  return p.search(authinfo).group(1)
