ab9233bb64c792fc96e2631373b7ab008c12daaa
ryane <ryanesc@gmail.com>
2014-01-21 08:35:44 -0500

    windows dotfiles install script

---
+++ install.bat
@@ -0,0 +1,5 @@
+REM this kind of blows but I don't use windows that much
+copy gitconfig %USERPROFILE%\.gitconfig
+copy githelpers %USERPROFILE%\.githelpers
+copy gitignore %USERPROFILE%\.gitignore
+copy vimrc %USERPROFILE%\.vimrc
