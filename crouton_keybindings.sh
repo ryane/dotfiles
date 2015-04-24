3de9fc16b489b9d05e28ffbdf83a9ee7a170efa9
ryane <ryanesc@gmail.com>
2015-04-24 11:10:44 -0400

    fix crouton keybindings script

--- crouton_keybindings.sh
+++ crouton_keybindings.sh
@@ -2,6 +2,7 @@ if type croutonversion &>/dev/null; then
   #If this is a crouton chroot, then swap left ctrl and search keys
   xmodmap -e "keycode 133 = Control_L"
   xmodmap -e "keycode 37 = Overlay1_Enable"
+  xmodmap -e "remove mod4 = Control_L"
   xmodmap -e "add control = Control_L"
   xmodmap -e "remove control = Overlay1_Enable"
 fi
