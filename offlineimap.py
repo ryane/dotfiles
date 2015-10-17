0e76c68becf696fd454542911c2b2fb08eac6569
ryane <ryanesc@gmail.com>
2015-10-17 08:50:23 -0400

    offlineimap tweaks

--- offlineimap.py
+++ offlineimap.py
@@ -2,14 +2,15 @@
 
 import re, os
 
-def get_imap_passwd():
-  cmd = "/usr/bin/security 2>&1 >/dev/null find-generic-password -s \"Gmail Offlineimap Application Password\" -g"
+def get_imap_passwd(name):
+  cmd = "/usr/bin/security 2>&1 >/dev/null find-generic-password -s \"%s\" -g" % (name)
   line = os.popen(cmd).readline()
-  passwds = line.split()
-  if len(passwds) == 2:
-    return passwds[1][1:-1]
-  else:
-    return ""
+  return line.replace('password: "', '').replace('"', '')
+  # passwds = line.split()
+  # if len(passwds) == 2:
+  #   return passwds[1][1:-1]
+  # else:
+  #   return ""
 
 def get_auth_info_pw(machine, login, port):
   s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
